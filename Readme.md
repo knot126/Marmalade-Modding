51a99e612f27b6c6aa99c29dc51c6823b9a8ead8
knot126 <73200930+knot126@users.noreply.github.com>
2024-06-10 23:44:11 +0000

    Add extended header hexdump

--- Readme.md
+++ Readme.md
@@ -5,3 +5,5 @@ Some misc. utilities made while modding games made with the Marmalade SDK. You c
 ## Tools
 
 * `dzextract.py`: Tries to extract a `.dz` archive and generate a DZip configuration file. There is only limited support ATM, so using the official `dzip.exe` is recommended.
+* `disable_sig_check.py`: Really basic script to patch out signature verification. Only works with Marmalade loader v4.40.0.
+* `dump_s3e_header.py`: Basic dump of an s3e file header.
--- dump_s3e_header.py
+++ dump_s3e_header.py
@@ -6,18 +6,42 @@ Print out basic info about a uncompressed s3e's header
 import struct
 import sys
 
+def hexdump(data, perline = 16):
+	"""
+	Generate a hexdump as a string
+	"""
+	
+	line = 0
+	buf = []
+	result = ""
+	
+	for d in data:
+		buf.append("{:02x}".format(d))
+		
+		if (len(buf) == perline):
+			result += "{:06x}  ".format(perline * line) + " ".join(buf) + "\n"
+			buf = []
+			line += 1
+	
+	if buf:
+		result += "{:06x}  ".format(perline * line) + " ".join(buf) + "\n"
+	
+	return result
+
 def main():
 	if (len(sys.argv) < 2):
 		print(f"Usage: {sys.argv[0]} <s3e file>")
 	
 	f = open(sys.argv[1], "rb")
-	mv = memoryview(f.read(0x40))
+	
+	# Basic header
+	basic_bytes = f.read(0x40)
+	mv = memoryview(basic_bytes)
 	header = mv.cast("I")
 	header_short = mv.cast("H")
-	f.close()
 	
 	print(f" *** BASIC HEADER *** ")
-	print(f"ident        = {hex(header[0])}")
+	print(f"ident        = {hex(header[0])} ({basic_bytes[0:4].decode('latin-1')})")
 	old_format = ((header[1] >> 0x10) & 0xff) == 0
 	if (old_format):
 		print(f"version      = {hex(header[1])} ({header[1] >> 12}.{header[1] & 0xff})")
@@ -42,6 +66,16 @@ def main():
 	print(f"baseAddrOrig = {hex(header[13])}")
 	print(f"extraOffset  = {hex(header[14])}")
 	print(f"extraSize    = {hex(header[15])}")
+	
+	# Extended header
+	print(f"\n *** EXTENDED HEADER *** ")
+	
+	ext_length = memoryview(f.read(4)).cast("I")[0]
+	print(f"extended header length = {hex(ext_length)}")
+	print(f"hexdump of extended header:")
+	print(hexdump(f.read(ext_length - 4)), end="")
+	
+	f.close()
 
 if (__name__ == "__main__"):
 	main()
